b3733f5a0d72141ff63ac7ef157df36c8706a3b4
ratnov22 <antonratnov@yandex.ru>
2024-10-28 22:49:53 +0300

    back-bot

---
+++ dist/user/user.controller.d.ts
@@ -0,0 +1,10 @@
+import { UserService } from './user.service';
+export declare class UserController {
+    private readonly userService;
+    constructor(userService: UserService);
+    getff(): Promise<import("@typegoose/typegoose").DocumentType<import("./user.model").UserModel, import("@typegoose/typegoose/lib/types").BeAnObject>[]>;
+    getProfile(_id: string): Promise<import("@typegoose/typegoose").DocumentType<import("./user.model").UserModel, import("@typegoose/typegoose/lib/types").BeAnObject>>;
+    updateProfile(_id: string, data: {
+        have_read: boolean;
+    }): Promise<boolean>;
+}
