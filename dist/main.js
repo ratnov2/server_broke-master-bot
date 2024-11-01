505697869b44b0114fa59a61ab302c0ef320f74b
ratnov22 <antonratnov@yandex.ru>
2024-11-01 13:52:20 +0300

    add https

--- dist/main.js
+++ dist/main.js
@@ -2,8 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const fs = require("fs");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    let httpsOptions = {};
+    if (process.env.IS_PRODUCTION === 'true') {
+        httpsOptions = {
+            key: fs.readFileSync('/etc/letsencrypt/live/chickentoken.ru/privkey.pem'),
+            cert: fs.readFileSync('/etc/letsencrypt/live/chickentoken.ru/fullchain.pem'),
+        };
+    }
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, { httpsOptions });
     app.setGlobalPrefix("api");
     app.enableCors();
     await app.listen(parseInt(process.env.PORT, 10) || 3000);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import * as fs from 'fs';
 
 async function bootstrap() {
- const app = await NestFactory.create(AppModule);
+    let httpsOptions = {}
+    if(process.env.IS_PRODUCTION === 'true'){
+        httpsOptions = {
+            key: fs.readFileSync('/etc/letsencrypt/live/chickentoken.ru/privkey.pem'),
+            cert: fs.readFileSync('/etc/letsencrypt/live/chickentoken.ru/fullchain.pem'),
+          };
+    }
+   
+
+const app = await NestFactory.create(AppModule,{ httpsOptions });
  app.setGlobalPrefix("api");
  app.enableCors()
  await app.listen(parseInt(process.env.PORT, 10) || 3000);
